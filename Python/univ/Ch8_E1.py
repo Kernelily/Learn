c93458985a60ed51d07e218cb87e03c022f71b79
Kernel <7.47lts@gmail.com>
2023-05-04 04:40:24 +0000

    Daily Practice

---
+++ Python/univ/Ch8_E1.py
@@ -0,0 +1,40 @@
+import os
+import time
+
+# 60 x 60 
+x = 0
+y = 0
+
+while x < 60:
+    os.system("clear")
+
+    print(f"{' ' * x} <#_#>")
+
+    x += 1
+    time.sleep(0.01)
+
+while y < 60:
+    os.system("clear")
+
+    print('\n' * y + ' ' * x + "<#_#>")
+
+    y += 1
+    time.sleep(0.01)
+
+while x >= 0:
+    os.system("clear")
+
+    print("\n" * y + " " * x + "<#_#>")
+
+    x -= 1
+    time.sleep(0.01)
+
+while y >= 0:
+    os.system("clear")
+
+    print("\n" * y + "<#_#>")
+
+    y -= 1
+    time.sleep(0.01)
+
+input("Press Enter to continue...")
